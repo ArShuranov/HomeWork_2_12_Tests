6380687c107fd34f5bd75601bd2fac47b347917d
ArShuranov <arshuranov@yandex.ru>
2023-06-06 20:44:37 +0700

    Initial commit

---
+++ src/test/java/com/example/homework_test/HomeWorkTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
